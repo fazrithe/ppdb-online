549e7a415bd9415e298730f92504e0d38ee8e590
MOH DEDE FAZRI <fazrithe@gmail.com>
2022-10-03 22:58:10 +0700

    Update form daftar
    Update form daftar

--- application/views/daftar.php
+++ application/views/daftar.php
@@ -1,88 +1,86 @@
 <!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>Sign Up Signin Form Template Example</title>
-  <link rel="stylesheet" href="<?= site_url('assets/signup/') ?>style.css">
-
-</head>
-<body>
-<!-- partial:index.partial.html -->
 <html lang="en">
+
 <head>
-  <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link href="<?= site_url('assets/backend/') ?>plugins/perfectscroll/perfect-scrollbar.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Responsive Admin Dashboard Template">
+	<meta name="keywords" content="admin,dashboard">
+	<meta name="author" content="stacks">
+	<!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<!-- Title -->
+	<title>DAFTAR AKUN PPDB <?= strtoupper($jenjang) ?></title>
+
+	<!-- Styles -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+	<link href="<?= site_url('assets/backend/') ?>plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?= site_url('assets/backend/') ?>plugins/perfectscroll/perfect-scrollbar.css" rel="stylesheet">
 	<link href="<?= site_url('assets/backend/') ?>plugins/pace/pace.css" rel="stylesheet">
 
 
 	<!-- Theme Styles -->
+	<link href="<?= site_url('assets/backend/') ?>css/main.min.css" rel="stylesheet">
 	<link href="<?= site_url('assets/backend/') ?>css/horizontal-menu/horizontal-menu.css" rel="stylesheet">
 	<link href="<?= site_url('assets/backend/') ?>css/custom.css" rel="stylesheet">
 
 	<link rel="icon" type="image/png" sizes="32x32" href="<?= site_url('assets/backend/') ?>images/neptune.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="<?= site_url('assets/backend/') ?>images/neptune.png" />
-  
+
 </head>
 
 <body>
-<div id="form">
-  <div class="container">
-    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-md-8 col-md-offset-2">
-      <div id="userform">
-		<div align="left" style="padding-left:10px; padding-top:10px;">
-          <img src="<?= site_url('assets/images/logo/SC-2209239550.png') ?>" alt="" width="15%" class="rounded mx-auto d-block img-fluid w-75"><a href="#signup"  role="tab" data-toggle="tab"><font size="5">PPDB <?= strtoupper($jenjang) ?></font></a>
+	<div class="app horizontal-menu app-auth-sign-up align-content-stretch d-flex flex-wrap justify-content-end">
+		<div class="app-auth-background">
+
 		</div>
-		  <div class="tab-content">
-          <div class="tab-pane fade active in" id="signup">
-		  <p class="auth-description">Login jika sudah punya akun. <a href="<?= site_url('login') ?>">Sign In</a></p>
-            <form id="signup">
-              <div class="row">
-                <div class="col-xs-12 col-sm-6">
-                  <div class="form-group">
-				  <select class="form-control m-b-md" name="jenjang" id="jenjang">
+		<div class="app-auth-container">
+			<div class="logo">
+				<a href="#">PPDB <?= strtoupper($jenjang) ?></a>
+			</div>
+			<p class="auth-description">Login jika sudah punya akun. <a href="<?= site_url('login') ?>">Sign In</a></p>
+
+			<div class="auth-credentials m-b-xl" id="blockui">
+				<form id="form">
+					<div class="row">
+						<div class="col-md-6 col-sm-6">
+							<label for="jenjang">Jenjang</label>
+							<select class="form-control m-b-md" name="jenjang" id="jenjang">
 								<?php foreach ($jurusan as $v) : ?>
 									<option value="<?= $v->id ?>" <?= $v->nama_jenjang == strtoupper($jenjang) ? "selected" : "" ?>><?= $v->nama_jenjang ?></option>
 								<?php endforeach ?>
 							</select>
-                    <p class="help-block text-danger"></p>
-                  </div>
-                </div>
-                <div class="col-xs-12 col-sm-6">
-                  <div class="form-group">
-				  <select class="form-control m-b-md" name="kelas" id="kelas" require>
-				  <option value="" selected>Kelas</option>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<label for="kelas">Kelas</label>
+							<select class="form-control m-b-md" name="kelas" id="kelas">
 								<?php foreach ($kelas as $v) : ?>
 									<option value="<?= $v->id_kelas ?>"><?= $v->nama_kelas ?></option>
 								<?php endforeach ?>
 							</select>
-                  </div>
-                </div>
-              </div>
-			  <div class="form-group">
-			  <label>Nama Lengkap<span class="req"></span> </label>
-			  <input type="email" class="form-control" id="nama" aria-describedby="nama" onBlur="javascript:{this.value = this.value.toUpperCase();}" placeholder="Nama Lengkap">
-              </div>
-              <div class="form-group">
-                <label>Tempat Lahir<span class="req">*</span> </label>
-                <input type="text" class="form-control m-b-md" id="tempat_lahir" aria-describedby="tempat_lahir" onBlur="javascript:{this.value = this.value.toUpperCase();}" placeholder="Tempat Lahir">
-                <p class="help-block text-danger"></p>
-              </div>
-			  <div class="form-group">
-                <label><span class="req"></span> </label>
-                <input type="date" class="form-control m-b-md" id="tanggal_lahir" aria-describedby="tanggal_lahir" placeholder="">
-                <p class="help-block text-danger"></p>
-              </div>
-			  <div class="form-group">
-                <label>Alamat<span class="req"></span> </label>
-                <textarea name="alamat" id="alamat" rows="3" class="form-control m-b-md" placeholder="Alamat Lengkap"></textarea>
-                <p class="help-block text-danger"></p>
-              </div>
-			  <div class="row">
-                <div class="col-xs-12 col-sm-6">
-                  <div class="form-group">
-				  <label><span class="req"></span> </label>
-				  <select name="agama" id="agama" class="form-control m-b-md">
+						</div>
+					</div>
+					<!-- <label for="nama" class="form-label">Nama</label> -->
+					<input type="email" class="form-control m-b-md" id="nama" aria-describedby="nama" onBlur="javascript:{this.value = this.value.toUpperCase();}" placeholder="Nama Lengkap">
+
+					<!-- <label for="tempat_lahir" class="form-label">Tempat Lahir</label> -->
+					<input type="text" class="form-control m-b-md" id="tempat_lahir" aria-describedby="tempat_lahir" onBlur="javascript:{this.value = this.value.toUpperCase();}" placeholder="Tempat Lahir">
+
+					<!-- <label for="tanggal_lahir" class="form-label">Tanggal Lahir</label> -->
+					<input type="date" class="form-control m-b-md" id="tanggal_lahir" aria-describedby="tanggal_lahir" placeholder="">
+
+
+					<!-- <label for="alamat" class="form-label">Alamat</label> -->
+					<textarea name="alamat" id="alamat" rows="3" class="form-control m-b-md" placeholder="Alamat Lengkap"></textarea>
+
+					<div class="row">
+						<div class="col-md-6">
+							<!-- <label for="agama">Agama</label> -->
+							<select name="agama" id="agama" class="form-control m-b-md">
 								<option value="">-- Pilih Agama --</option>
 								<option value="Islam">Islam</option>
 								<option value="Kristen">Kristen</option>
@@ -91,61 +89,40 @@
 								<option value="Buddha">Buddha</option>
 								<option value="Konghucu">Konghucu</option>
 							</select>
-                    <p class="help-block text-danger"></p>
-                  </div>
-                </div>
-                <div class="col-xs-12 col-sm-6">
-                  <div class="form-group">
-				  <label><span class="req"></span> </label>
-				  <select name="jk" class="form-control m-b-md" id="jk">
+						</div>
+						<div class="col-md-6">
+							<!-- <label for="jk">Jenis Kelamin</label> -->
+							<select name="jk" class="form-control m-b-md" id="jk">
 								<option value="">-- Pilih Jenis kelamin --</option>
 								<option value="L">Laki-laki</option>
 								<option value="K">Perempuan</option>
 							</select>
-                  </div>
-                </div>
-              </div>
-			  <div class="row">
-                <div class="col-xs-12 col-sm-6">
-			  <div class="form-group">
-                <p style="color:red"> Gunakan Email Aktif<span class="req">*</span> </p>
-                <input type="email" class="form-control m-b-md" id="email" aria-describedby="tanggal_lahir" placeholder="Email">
-                <p class="help-block text-danger"></p>
-              </div>
-				</div>
-				<div class="col-xs-12 col-sm-6">
-              <div class="form-group">
-                <p style="color:red"> Gunakan awalan 6281xxxxxxx<span class="req">*</span> </p>
-                <input type="text" class="form-control m-b-md" id="phone" aria-describedby="tanggal_lahir" placeholder="Gunakan awalan 6281xxxxxxx">
-                <p class="help-block text-danger"></p>
-              </div>
-				</div>
-			  </div>
-              <div class="mrgn-30-top">
-			  <div class="auth-submit">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6"><small class="text-danger">Gunakan Email Aktif</small>
+							<input type="email" class="form-control m-b-md" id="email" aria-describedby="tanggal_lahir" placeholder="Email">
+							
+						</div>
+						<div class="col-md-6"><small class="text-danger">Gunakan awalan 6281xxxxxxx</small>
+							<input type="text" class="form-control m-b-md" id="phone" aria-describedby="tanggal_lahir" placeholder="6289xxxxxxx">
+							
+						</div>
+					</div>
+			</div>
+			<div class="auth-submit">
 				<div id="loading" class="text-center" style="display: none;">
 					<div class="loader"></div>
 					<span>Loading ...</span>
 				</div>
 				<button type="button" id="btn_add" class="btn btn-primary">Daftar Sekarang</button>
 			</div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- /.container --> 
-</div>
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</body>
-<!-- partial -->
-  <script  src="<?= site_url('assets/signup/') ?>script.js"></script>
-<!-- Javascripts -->
-<script src="<?= site_url('assets/backend/') ?>plugins/jquery/jquery-3.5.1.min.js"></script>
+			</form>
+		</div>
+	</div>
+
+	<!-- Javascripts -->
+	<script src="<?= site_url('assets/backend/') ?>plugins/jquery/jquery-3.5.1.min.js"></script>
 	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<script src="<?= site_url('assets/backend/') ?>plugins/bootstrap/js/bootstrap.min.js"></script>
 	<script src="<?= site_url('assets/backend/') ?>plugins/perfectscroll/perfect-scrollbar.min.js"></script>
@@ -205,11 +182,11 @@
 
 						},
 						error: function(data) {
-							// Swal.fire(
-							// 	'Sorry',
-							// 	data,
-							// 	'warning'
-							// )
+							Swal.fire(
+								'Sorry',
+								data,
+								'warning'
+							)
 						}
 					});
 
@@ -219,4 +196,5 @@
 		})
 	</script>
 </body>
+
 </html>
